092435d2de14f2fb0f282efe69cdc5f87bcffd5c
nkumnick <nkumnick@oev.de>
2023-04-20 15:47:25 +0200

    adjust vercel.json

--- api/app.ts
+++ api/app.ts
@@ -5,3 +5,25 @@ export default async (req: any, res: any) => {
   await apiApp.ready();
   apiApp.server.emit('request', req, res);
 };
+
+// "use strict";
+//
+// // Read the .env file.
+// import * as dotenv from "dotenv";
+// dotenv.config();
+//
+// // Require the framework
+// import Fastify from "fastify";
+//
+// // Instantiate Fastify with some config
+// const app = Fastify({
+//   logger: true,
+// });
+//
+// // Register your application as a normal plugin.
+// app.register(apiApp);
+//
+// export default async (req, res) => {
+//   await app.ready();
+//   app.server.emit('request', req, res);
+// }
